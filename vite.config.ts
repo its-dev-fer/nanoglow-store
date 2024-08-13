7d447fba2b2d469608de1eba298376fd6e909235
Chanona <chanonaa.me@gmail.com>
2024-08-12 20:22:11 -0600

    Agregar configuracion de PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {VitePWA} from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      manifest: {
+        display: 'standalone',
+        display_override: ['window-controls-overlay'],
+        lang: 'es-MX',
+        name: 'Nanoglow',
+        short_name: 'Nanoglow',
+        description: 'Proyecto Nanoglow',
+        theme_color: '#ffffff', //Depende del color principal del proyecto
+        background_color: '#ffffff', //Depend del color establecido en el proyecto
+        // icons: [],
+      }
+    })
+  ],
 })
