c3652ec3d950d9e275c99406c74b99cca670e733
MrYen2-0 <223225@ids.upchiapas.edu.mx>
2024-08-15 13:26:34 -0600

    Condiciones y restricciones en el correo y en la contraseñá

--- src/components/Formulario-login/LoginForm.tsx
+++ src/components/Formulario-login/LoginForm.tsx
@@ -1,28 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/LoginForm.css';
 
 const LoginForm: React.FC = () => {
-  
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
- 
-    alert('Inicio sesion con exito');
+
+    if (!/\S+@\S+\.\S+/.test(email)) {
+      alert('Por favor, introduce un correo electrónico válido.');
+      return;
+    }
+
+    alert('Inicio de sesión con éxito');
   };
 
   return (
     <div className="login">
       <h1>Bienvenido</h1>
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>Usuario:</label>
-        <input type="text"/>
-      </div>
-      <div>
-        <label>Contraseña:</label>
-        <input type="password"/>
-      </div>
-      <button type="submit">Login</button>
-    </form>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Correo Electrónico:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            placeholder="tucorreo@ejemplo.com"
+          />
+        </div>
+        <div>
+          <label>Contraseña:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            minLength={6}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 };
