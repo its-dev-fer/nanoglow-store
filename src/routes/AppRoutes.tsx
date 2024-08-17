9cdb4f571a24990e1d7006c9c443b3eba35c3b54
MrYen2-0 <223225@ids.upchiapas.edu.mx>
2024-08-17 11:34:45 -0600

    Version 0.0.3

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,12 +1,13 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginForm from "../components/Formulario-login/LoginForm";
+import Hero3D from '../components/hero3D/Hero3D';
+import LoginForm from '../components/Formulario-login/LoginForm';
 
-const AppRoutes: React.FC = () => {
+const AppRoutes = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginForm />} />
+        <Route path="/" element={<Hero3D />} />
+        <Route path="/login" element={<LoginForm />} />
       </Routes>
     </Router>
   );
