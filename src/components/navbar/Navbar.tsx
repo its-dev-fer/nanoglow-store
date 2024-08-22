9b1f02411eb62a4d545ae9da8dc12efb6ecec7a2
MrYen2-0 <223225@ids.upchiapas.edu.mx>
2024-08-21 20:17:53 -0600

    Coreccion de imagenes #2

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -4,7 +4,7 @@ import logo from '../../assets/logo.png';
 import lupaIcon from '../../assets/Lupa.svg';
 import xIcon from '../../assets/X.svg';
 import bolsaIcon from '../../assets/bolsa.svg';
-import userIcon from '../../assets/user.svg'; 
+import userIcon from '../../assets/user.svg';
 
 const Navbar: React.FC = () => {
   const [isSearchVisible, setIsSearchVisible] = useState(false);
@@ -23,31 +23,28 @@ const Navbar: React.FC = () => {
             className="w-16 h-16 md:w-14 md:h-14"
           />
         </Link>
-
         {!isSearchVisible && (
           <div className="flex-grow mx-4 flex justify-center space-x-4">
-            <Link to="/impresoras" className="text-black-300 hover:text-white">
+            <Link to="/impresoras" className="text-black-300 hover:text-purple-600">
               Impresoras
             </Link>
-            <Link to="/modelos3d" className="text-black-300 hover:text-white">
+            <Link to="/modelos3d" className="text-black-300 hover:text-purple-600">
               Modelos 3D
             </Link>
-            <Link to="/filamentos" className="text-black-300 hover:text-white">
+            <Link to="/filamentos" className="text-black-300 hover:text-purple-600">
               Filamentos
             </Link>
-            <Link to="/cursos" className="text-black-300 hover:text-white">
+            <Link to="/cursos" className="text-black-300 hover:text-purple-600">
               Cursos
             </Link>
           </div>
         )}
-
         {isSearchVisible && (
           <div className="flex-grow mx-4">
             <input
               type="text"
               placeholder="Buscar..."
-              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
-            />
+              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"/>
           </div>
         )}
 
@@ -56,21 +53,18 @@ const Navbar: React.FC = () => {
             <img
               src={isSearchVisible ? xIcon : lupaIcon}
               alt={isSearchVisible ? "Cerrar" : "Buscar"}
-              className={isSearchVisible ? "w-4 h-4 md:w-6 md:h-6" : "w-6 h-6 md:w-8 md:h-8"}
-            />
+              className={isSearchVisible ? "w-4 h-4 md:w-6 md:h-6" : "w-6 h-6 md:w-8 md:h-8"}/>
           </div>
           <img
             src={bolsaIcon}
             alt="Bolsa de compras"
-            className="w-6 h-6 md:w-8 md:h-8"
-          />
+            className="w-6 h-6 md:w-8 md:h-8"/>
           <div className="flex flex-col items-center">
             <img
               src={userIcon} 
               alt="User Icon"
-              className="w-6 h-6 md:w-8 md:h-8 mb-1" 
-            />
-            <Link to="/login" className="text-black-300 hover:text-white">
+              className="w-6 h-6 md:w-8 md:h-8 mb-1"/>
+            <Link to="/login" className="text-black-300 hover:text-purple-600">
               Inicio Sesión
             </Link>
           </div>
