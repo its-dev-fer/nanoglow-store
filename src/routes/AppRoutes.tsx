38796f694c7cc1e1377518bccaa762e53721a69a
Alejandro Chanona <122000806+DazhZs@users.noreply.github.com>
2024-08-26 19:46:36 -0600

    Agregar componente de actualizacion (#10)

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Hero3D from "../components/hero3D/Hero3D";
 import RegisterForm from "../components/formulario-registro/RegisterForm";
-import LoginForm from '../components/Formulario-login/LoginForm';
-import Navbar from '../components/navbar/Navbar';
-
+import LoginForm from "../components/Formulario-login/LoginForm";
+import Navbar from "../components/navbar/Navbar";
+import UpdateForm from "../components/formulario-actualizacion/UpdateForm";
 
 const AppRoutes: React.FC = () => {
   return (
@@ -13,6 +13,7 @@ const AppRoutes: React.FC = () => {
         <Route path="/" element={<Hero3D />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/register" element={<RegisterForm />} />
+        <Route path="/profile" element={<UpdateForm />} />
       </Routes>
     </Router>
   );
